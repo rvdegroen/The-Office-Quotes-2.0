bbfa583b4c5049b0d479a7f7624bfcb36c78592d
rvdegroen <rvdegroen@gmail.com>
2023-03-27 16:30:37 +0200

    tried to fetch but doesn't work

--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,23 @@
 // IMPORTS
 import express from "express";
+import fetch from "node-fetch";
 
 // VARIABLES
 export const router = express.Router();
 
-// homepage
-router.get("/", (reg, res) => {
-	res.json({
-		//fetch api
-	});
-});
+// game
+router.get("/game", async (req, res) => {
+	try {
+		// fetch random quote
+	  const response = await fetch("https://www.officeapi.dev/api/quotes/random");
+	  const data = await response.json();
+	  res.json(data);
+	  console.log(data);
 
+	} catch (error) {
+	  console.error(error);
+	  res.status(500).send('Error fetching data');
+	}
+  });
+
+  
