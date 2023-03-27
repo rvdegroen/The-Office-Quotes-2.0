822f6f66b286c4ce7e429a63e811038c04aa22bc
rvdegroen <rvdegroen@gmail.com>
2023-03-27 17:03:18 +0200

    fetching succesful

--- routes/api.js
+++ routes/api.js
@@ -5,13 +5,13 @@ import fetch from "node-fetch";
 // VARIABLES
 export const router = express.Router();
 
-// game
+// fetched on localhost:3000/api/game
 router.get("/game", async (req, res) => {
 	try {
 		// fetch random quote
 	  const response = await fetch("https://www.officeapi.dev/api/quotes/random");
 	  const data = await response.json();
-	  res.json(data);
+	  res.json(data.data);
 	  console.log(data);
 
 	} catch (error) {
