8b3685ba93e593b651ab9fdd6bd73222936f74e1
rvdegroen <rvdegroen@gmail.com>
2023-04-02 16:08:36 +0200

    changed filename

---
+++ public/scripts/game.js
@@ -0,0 +1,15 @@
+console.log("hi I'm main bitch");
+
+// function when the user makes a correct or wrong choice on /game page
+// global scope is the window object and this is only for browsers, otherwise it won't recognize the function in my game.ejs
+window.correctChoice = function () {
+	alert("That was correct!");
+	// refresh the page
+	location.reload();
+};
+
+window.wrongChoice = function (speaker) {
+	alert(`That was wrong! The correct answer was ${speaker}!`);
+	// refresh the page
+	location.reload();
+};
