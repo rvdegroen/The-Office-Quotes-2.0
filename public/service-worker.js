88cc41cd3c0fd9a94332e66456470402678d23b5
rvdegroen <rvdegroen@gmail.com>
2023-04-06 13:35:47 +0200

    PLSSSSSSSSS WORK

--- public/service-worker.js
+++ public/service-worker.js
@@ -12,6 +12,8 @@ self.addEventListener("install", function (e) {
 				"/fonts/work-sans-v18-latin-regular.woff2",
 				"/scripts/main.js",
 				"/images/offline.png",
+				"/",
+				"/index.html",
 			]);
 		})
 	);
