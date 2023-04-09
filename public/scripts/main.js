5fe7fa2a54d5bd8f7d92d9e11c770189f98ef435
rvdegroen <rvdegroen@gmail.com>
2023-04-09 22:58:47 +0200

    test

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,10 +1,13 @@
 // src: https://web.dev/service-workers-registration/
+console.log("This is so stupid.");
 
 if ("serviceWorker" in navigator) {
 	window.addEventListener("load", function () {
 		navigator.serviceWorker.register("/service-worker.js");
 	});
 }
+
+fetch("https://officeapi.dev/api/quotes");
 
 // if (!navigator.onLine) {
 // 	const img = document.createElement("img");
