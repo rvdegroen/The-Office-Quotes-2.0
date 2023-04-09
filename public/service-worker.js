b8650c8959bada5dfa9a98bc88cc903a5ab014b6
rvdegroen <rvdegroen@gmail.com>
2023-04-09 21:13:10 +0200

    added offline in cache to test

--- public/service-worker.js
+++ public/service-worker.js
@@ -15,6 +15,7 @@ self.addEventListener("install", function (e) {
 					"/fonts/work-sans-v18-latin-regular.woff2",
 					"/style/style.css",
 					"/style/variables.css",
+					"/pages/offline".
 				]);
 			})
 			.catch(function (error) {
