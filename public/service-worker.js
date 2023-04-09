b49e2ceb174c0392003fd2977175b8dd16061161
rvdegroen <rvdegroen@gmail.com>
2023-04-09 23:12:56 +0200

    added comment

--- public/service-worker.js
+++ public/service-worker.js
@@ -44,6 +44,15 @@ self.addEventListener("fetch", function (event) {
 					})
 				);
 			})
+
+			// 			.catch(function (error) {
+			// 				console.error("Error fetching the resource:", error);
+			// 				// Return an offline fallback page or a custom error response
+			// 				return caches.match("/offline/offline.html");
+			// 			})
+			// 	);
+			// });
+
 			.catch(function (error) {
 				console.error("Error fetching the resource:", error);
 				// src: https://gist.github.com/felquis/7e149e1db16aa57b1354
